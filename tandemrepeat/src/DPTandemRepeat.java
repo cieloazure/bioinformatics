50962088017537176d23f84a03f6c116493f3faf
tusharkaley <tushar.K197@gmail.com>
2019-11-27 18:07:23 -0500

    Updated to push repeats to a hashmap

--- tandemrepeat/src/DPTandemRepeat.java
+++ tandemrepeat/src/DPTandemRepeat.java
@@ -1,15 +1,30 @@
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
 
 public class DPTandemRepeat {
     char [] sequence;
     char pointers [][];
     int matrix [][];
-    public DPTandemRepeat(char [] sequence){
-        this.sequence = sequence;
-        this.pointers = new char [sequence.length][sequence.length];
-        this.matrix = new int [sequence.length][sequence.length];
+    HashMap <String, ArrayList<String>> repeats;
+
+    private DPTandemRepeat(String sequence){
+        this.setSequence(sequence);
+        this.pointers = new char [this.sequence.length][this.sequence.length];
+        this.matrix = new int [this.sequence.length][this.sequence.length];
+        this.repeats = new <String, ArrayList<String>>HashMap();
     }
+
+    private void setSequence(String sequence) {
+        char [] seq_char = new char[sequence.length()+1];
+        char [] seq_char_temp = sequence.toCharArray();
+        seq_char[0] = '1';
+        for (int i =1; i<seq_char.length;i++){
+            seq_char[i] = seq_char_temp[i-1];
+        }
+        this.sequence = seq_char;
+    }
+
     public void buildMatrix(){
 
 
@@ -76,10 +91,11 @@ public class DPTandemRepeat {
         }
         return op;
     }
-
     public void buildTandemRepeats(int i, int j){
         String seq_1= "";
         String seq_2= "";
+        int i_str = i;
+        int j_str = j;
         while(this.pointers[i][j] != 's'){
 
             if(this.pointers[i][j] == 'd'){
@@ -92,35 +108,44 @@ public class DPTandemRepeat {
                 seq_1 = "-"+seq_1 ;
                 seq_2 = this.sequence[i]+ seq_2 ;
                 i=i-1;
+                return;
 
             }
             else if (this.pointers[i][j] == 'l'){
                 seq_1 = this.sequence[j]+seq_1;
                 seq_2 = "-"+ seq_2 ;
                 j=j-1;
+                return;
             }
 
         }
-        System.out.println(seq_1);
-        System.out.println(seq_2);
+        ArrayList<String> itemsList = this.repeats.get(seq_1);
+
+        // if list does not exist create it
+        if(itemsList == null) {
+            itemsList = new ArrayList<String>();
+            itemsList.add((i+1)+":"+i_str);
+            itemsList.add((j+1)+":"+j_str);
+            this.repeats.put(seq_1, itemsList);
+        } else {
+            // add if item is not already in list
+            String pos = (i+1)+":"+i_str;
+            if(!itemsList.contains(pos)) itemsList.add(pos);
+            pos = (j+1)+":"+j_str;
+            if(!itemsList.contains(pos)) itemsList.add(pos);
+
+        }
+
+//        System.out.println(seq_1+": Starting at "+(i+1)+ " ending at "+i_str);
+//        System.out.println(seq_2+": Starting at "+(j+1)+ " ending at "+j_str);
+//        System.out.println("**************");
     }
     public static void main(String[] args){
-        String seq = "aactaat";
+        String seq = "ATTCGATTCGATTCG";
 
-        char [] seq_char = new char[seq.length()+1];
-        char [] seq_char_temp = seq.toCharArray();
-        seq_char[0] = '1';
-        for (int i =1; i<seq_char.length;i++){
-            seq_char[i] = seq_char_temp[i-1];
-        }
-        DPTandemRepeat dptr = new DPTandemRepeat(seq_char);
+        DPTandemRepeat dptr = new DPTandemRepeat(seq);
         dptr.buildMatrix();
-        for(int i = 0; i < seq_char.length; i++) {
-            for (int j = 0; j < seq_char.length; j++) {
-                System.out.print(dptr.matrix[i][j]+"\t");
-            }
-            System.out.println();
-        }
+
         for(int i = 0; i < dptr.sequence.length; i++) {
             for (int j = 0; j < dptr.sequence.length; j++) {
                 System.out.print(dptr.pointers[i][j]+"\t");
@@ -129,15 +154,14 @@ public class DPTandemRepeat {
         }
         for(int i = 0; i < dptr.sequence.length; i++) {
             for (int j = i; j < dptr.sequence.length; j++) {
-                if(dptr.matrix[i][j]>0){
+                if(dptr.matrix[i][j]>0 && dptr.sequence[i]==dptr.sequence[j]){
                     dptr.buildTandemRepeats(i,j);
-                    System.out.println("**************");
+
                 }
 
             }
 
         }
-
     }
 
 
