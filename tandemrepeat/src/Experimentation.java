0f20bfb683ec335e25b2f817383d2eadf2901494
Akash S <akash.shingte@gmail.com>
2019-12-04 13:57:37 -0500

    Fixed equality and hashcode conditions of TROutput

--- tandemrepeat/src/Experimentation.java
+++ tandemrepeat/src/Experimentation.java
@@ -1,4 +1,11 @@
 public class Experimentation {
-    public static void main(String[] args) {
+    int seqLenLL = 20;
+    int seqLenUL = 100;
+    int rLenLL = 3;
+    int rLenUL = 100;
+    int rInstancesLL = 2;
+    int rInstancesUL = 10;
+
+    public void varyingTotalSequenceLength() {
     }
 }
--- tandemrepeat/src/Result.java
+++ tandemrepeat/src/Result.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 public class Result {
 
@@ -36,7 +33,7 @@ public class Result {
             StringBuffer opt = new StringBuffer();
             opt.append("[");
             for (TROutput t : e.getValue()) {
-                opt.append(" (" + t.idx + "," + t.length + "," + t.repeats + "),");
+                opt.append(" (").append(t.idx).append(",").append(t.length).append(",").append(t.repeats).append("),");
             }
             opt.deleteCharAt(opt.length() - 1);
             opt.append("]");
@@ -45,4 +42,52 @@ public class Result {
 
         System.out.println("Execution time: " + this.executionTime + " ms");
     }
+
+    public boolean equalTRs(Result other) {
+
+        if(other.results.size() != this.results.size()) return false;
+
+        for(String key: this.results.keySet()) {
+            if(!other.results.containsKey(key)) return false;
+        }
+        return true;
+    }
+//    TODO: Merge results in case of suffix tree when we have tandem repeats of length 2
+//    public void merge() {
+//        class Interval {
+//            int startIdx;
+//            int endIdx;
+//            int length;
+//            int repeats;
+//            int primitiveLength;
+//        }
+//
+//        for(String key: this.results.keySet()) {
+//            Set<TROutput> val1 = this.results.get(key);
+//        }
+//    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) return true;
+        if(!(obj instanceof Result)) return false;
+
+        Result other = (Result)obj;
+        if(other.results.size() != this.results.size()) return false;
+
+        for(String key: this.results.keySet()) {
+            if(!other.results.containsKey(key)) return false;
+
+            Set<TROutput> val1 = this.results.get(key);
+            Set<TROutput> val2 = other.results.get(key);
+
+            if(val1.size() != val2.size()) return false;
+
+            if(!val1.containsAll(val2)) return false;
+        }
+
+        return true;
+    }
+
+
 }
